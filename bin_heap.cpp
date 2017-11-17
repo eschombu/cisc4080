cf3d4247701e4e5c70ef56721537d4d40d84ed2f
Erik Schomburg <erik@cognescent.com>
2017-11-17 08:47:49 -0500

    Added better BinaryHeap testing script in main function of bin_heap.cpp

--- bin_heap.cpp
+++ bin_heap.cpp
@@ -8,28 +8,37 @@ using namespace std;
 void test_BinHeap() {
     BinaryHeap<string> H;
 
+    // User builds heap by entering values and keys
     cout << "Input items and their keys (separated by whitespace) to insert "
-            "into the min-heap.\n";
+            "into the min-heap. If an item is alread in the heap, the key "
+            "will be updated.\n";
     cout << "Hitting 'Enter' with no input stops building the heap and "
-            "iteratively pops the minimum key value until it is empty.\n\n";
-    cout << "(Item, Key): ";
+            "iteratively pops the minimum key value until it is empty.\n";
+    cout << "\n(Item, Key): ";
     string line, item;
     int key;
     
     while (getline(cin, line) && !line.empty()) {
         istringstream line_stream (line);
         line_stream >> item >> key;
-        H.insert(item, key);
-        cout << "(Item, Key): ";
+        if (H.get_position(item) > H.size()) {
+            H.insert(item, key);
+        } else {
+            H.update_key(item, key);
+        }
+        cout << "Heap:" << endl;
+        H.print();
+        cout << "\n(Item, Key): ";
     }
     cout << endl;
 
-    cout << "Heap:" << endl;
-    H.print();
+    cout << "Popping items until empty..." << endl;
     while (H.size() > 0) {
         cout << "Pop min: " << H.pop_min() << endl;
+        cout << "Heap:" << endl;
         H.print();
     }
+    cout << endl;
 }
 
 int main() {
